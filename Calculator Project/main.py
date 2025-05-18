995a5e2940b39a428a7d94a2716c4dbbb2c8a4dd
gsramos1305 <gagabrielramos.londrina@gmail.com>
2025-05-18 12:56:27 -0400

    calculator project

---
+++ Calculator Project/main.py
@@ -0,0 +1,46 @@
+from importlib.resources import open_text
+
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
+
+# print(operations['+'](2, 3))
+
+def calculator():
+    should_continue = True
+    num1 = float(input('First Number: '))
+
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input('Pick an operation: ')
+        num2 = float(input('Second Number: '))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"y to continue calculating with {answer}, n to stop: ")
+        if choice == 'y':
+            num1 = answer
+        else:
+            print("\n" * 20)
+            should_continue = False
+            calculator()
+
+
+calculator()
