c62a8d1d31d7a73793d559daf2ee19cd5cf9e749
Gabriel <gagabrielramos.londrina@gmail.com>
2025-05-23 13:01:49 -0400

    arquivo main atualizado

---
+++ Blackjack Project/task/main.py
@@ -0,0 +1,48 @@
+import random
+from operator import truediv
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_scores(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        user_hand.remove(11)
+        user_hand.append(1)
+    if sum(cards) == 21:
+        return 0
+    else:
+        return sum(cards)
+
+
+#----------------------------------------------------------------------------
+
+user_hand = []
+computer_hand = []
+game_over = False
+
+
+for n in range(2):
+    user_hand.append(deal_card())
+    computer_hand.append(deal_card())
+while not game_over:
+    user_score = calculate_scores(user_hand)
+    computer_score = calculate_scores(computer_hand)
+    print(f"Your hand: {user_hand}, current score: {user_score}")
+    print(f"Computer's first hand: {computer_hand[0]}")
+
+    if user_score == 0 or user_score > 21:
+        game_over = True
+    else:
+        deal_new_card = input("Hit or Stop?: ").lower()
+        if deal_new_card == "hit":
+            user_hand.append(deal_card())
+            calculate_scores(user_hand)
+            user_score = sum(user_hand)
+            print(f"Your hand: {user_hand}\ncurrent score: {user_score}")
+        else:
+            gamer_over = True
