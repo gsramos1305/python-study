0a81887b14cdaec575fa97ae83b71fa5b1f73676
Gabriel <gagabrielramos.londrina@gmail.com>
2025-06-04 13:01:55 -0400

    commit

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,9 +1,31 @@
-from menu import MENU
+from menu import MENU, resources
 
-machine_working = True
-while machine_working:
-    ask_user = input("What would you like? (espresso/latte/capuccino): ")
-    for n in MENU[ask_user]["ingredients"]:
-        print(f"{n}: {MENU[ask_user]['ingredients'][n]}ml")
+
+def is_resources_enough(drink_ingredients):
+    for item in drink_ingredients:
+        if drink_ingredients[item] >= resources[item]:
+            print("Sorry, insufficient ingredients.")
+
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/capuccino/report/off): ").lower()
+    confirm = input("Confirm? y/n: ")
+    if confirm == 'y':
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            for n in resources:
+                print(f"{n}: {resources[n]}")
+        elif choice == "espresso" or "latte" or "cappuccino":
+            for n in MENU[choice]["ingredients"]:
+                print(f"{n}: {MENU[choice]['ingredients'][n]}ml")
+            drink = MENU[choice]["ingredients"]
+
+
+
+
+
+
 
 
