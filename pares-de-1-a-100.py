a2ff5f5c6a51f34ce021d0a32fe81cb513a045eb
Gabriel <gagabrielramos.londrina@gmail.com>
2025-06-05 12:03:15 -0400

    repository updated

--- pares-de-1-a-100.py
+++
@@ -1,7 +0,0 @@
-pares = []
-
-for n in range(1, 101):
-    if n % 2 == 0:
-        pares.append(n)
-
-print(pares)
--- tabuada.py
+++
@@ -1,5 +0,0 @@
-number = int(input('Type a number: \n'))
-
-for i in range(1, 11):
-    result = i * number
-    print(f"{number} x {i} = {result}")
