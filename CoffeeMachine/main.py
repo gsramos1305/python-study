0ba9d63a673a143982be8b6aab5a404cb6369d5d
Gabriel <gagabrielramos.londrina@gmail.com>
2025-06-05 12:00:27 -0400

    Cofffe Machine 100%

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -4,23 +4,58 @@ from menu import MENU, resources
 def is_resources_enough(drink_ingredients):
     for item in drink_ingredients:
         if drink_ingredients[item] >= resources[item]:
-            print("Sorry, insufficient ingredients.")
+            print(f"Sorry, insufficient {item}.")
+            return False
+    return True
 
+def process_coins():
+    print("---Insert coins---")
+    total = int(input("Quarters: ")) * 0.25
+    total += int(input("Dimes: ")) * 0.10
+    total += int(input("Nickels: ")) * 0.05
+    total += int(input("Pennies: ")) * 0.01
+    return total
+
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = money_received - drink_cost
+        print(f"\nYour change: ${change:.2f}")
+        return True
+    else:
+        print("Not enough money.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}!")
+    print("\n"*3)
 
 is_on = True
 while is_on:
-    choice = input("What would you like? (espresso/latte/capuccino/report/off): ").lower()
-    confirm = input("Confirm? y/n: ")
-    if confirm == 'y':
-        if choice == "off":
-            is_on = False
-        elif choice == "report":
-            for n in resources:
-                print(f"{n}: {resources[n]}")
-        elif choice == "espresso" or "latte" or "cappuccino":
-            for n in MENU[choice]["ingredients"]:
-                print(f"{n}: {MENU[choice]['ingredients'][n]}ml")
-            drink = MENU[choice]["ingredients"]
+    print("MENU: ")
+    for drink in MENU:
+        print(f"-{drink}: ${MENU[drink]['cost']}")
+    choice = input("\nWhat would you like?: ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        for n in resources:
+              print(f"{n}: {resources[n]}")
+    elif choice == "espresso" or "latte" or "cappuccino":
+          #for n in MENU[choice]["ingredients"]:
+              #print(f"{n}: {MENU[choice]['ingredients'][n]}ml")
+          drink = MENU[choice]
+          if is_resources_enough(drink["ingredients"]):
+              user_money = process_coins()
+              print(f"\nMoney inserted: ${user_money:.2f}")
+              confirm = input("Confirm? y/n: ")
+              if confirm == 'y':
+                  is_transaction_successful(user_money, drink["cost"])
+                  make_coffee(choice, drink["ingredients"])
+
+
+
 
 
 
