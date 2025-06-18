ded6da2d09aa07c210ef27cebb55ebaa628f27ac
Gabriel <gagabrielramos.londrina@gmail.com>
2025-06-18 12:24:02 -0400

    path to image hirst painting

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,4 +1,12 @@
 import colorgram
 
-colors = colorgram.extract('test.jpg', 6)
-print(colors)
+rgb_colors = []
+colors = colorgram.extract('hirst-painting/test.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+    
+print(rgb_colors)
