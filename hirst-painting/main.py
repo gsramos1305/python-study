488e232787d472df17ab35df743e0121f6d762f3
gsramos1305 <gagabrielramos.londrina@gmail.com>
2025-06-20 00:27:30 -0400

    hirst painting 99%

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,12 +1,48 @@
 import colorgram
+import turtle as t
+import random
+
+tu = t.Turtle()
+tu.speed('fastest')
+tu.width(1)
+t.colormode(255)
+
+
+
+
+def next_line(radius, height):
+    tu.home()
+    tu.left(90)
+    # tu.forward(radius)
+    tu.forward(((radius * 2) * 1.50) * (height))
+    tu.right(90)
+
+def paint(radius, circles_per_line, height):
+    for n in range(1, height + 1):
+        for _ in range(circles_per_line):
+            tu.setheading(0)
+            tu.down()
+            tu.dot(radius, random.choice(rgb_colors))
+            tu.up()
+            tu.forward((radius * 2) * 1.50)
+        next_line(radius, n)
+        
+
+
 
 rgb_colors = []
-colors = colorgram.extract('hirst-painting/test.jpg', 30)
+colors = colorgram.extract('hirst-painting/test.jpg', 25)
 for color in colors:
     r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
     new_color = (r, g, b)
     rgb_colors.append(new_color)
-    
-print(rgb_colors)
+
+color = random.choice(rgb_colors)
+print(color)
+
+paint(15, 5, 5)
+
+screen = t.Screen()
+screen.exitonclick()
